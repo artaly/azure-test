38dba69b76aa970e1c8e902c339e6f5e8564fbb7
artaly <villalonalycah@gmail.com>
2023-03-13 16:17:07 +0800

    Fix

--- src/index.js
+++ src/index.js
@@ -4,15 +4,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const http = require('http');
-const port = process.env.PORT || 3000;
 
-http.createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World!');
-}).listen(port);
-
-console.log(`Server running at http://localhost:${port}`);
 root.render(
   <React.StrictMode>
     <App />
